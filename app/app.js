2f7c5591e8fa444bc02339df29e16cc77ca76211
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-01-20 00:25:23 +0300

    Join config in one

--- app/app.js
+++ app/app.js
@@ -1,21 +1,19 @@
 'use strict';
 
-angular.module('myApp', [
-  'ngRoute',
-  'selectize',
-  'ng-token-auth',
-  'angular-loading-bar',
-  'ngAnimate'
-])
-
-  .config(function ($authProvider) {
+angular
+  .module('myApp', [
+    'ngRoute',
+    'selectize',
+    'ng-token-auth',
+    'angular-loading-bar',
+    'ngAnimate'
+  ])
+  .config(function ($authProvider, cfpLoadingBarProvider) {
     $authProvider.configure({
       apiUrl: 'http://localhost:8080',
       emailRegistrationPath: '/signup',
       emailSignInPath: '/signin',
       signOutUrl: ' '
     });
-  })
-  .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
     cfpLoadingBarProvider.latencyThreshold = 1000;
-  }]);
+  })
