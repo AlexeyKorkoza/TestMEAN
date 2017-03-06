5b817af869a44ca4b17bcfd7b6f03e1fe92340ff
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-03-06 09:58:10 +0300

    Fix route to setting user

--- routers/users.js
+++ routers/users.js
@@ -3,18 +3,23 @@ var router = express.Router();
 var userModel = require('../models/user');
 var mongoose = require('mongoose');
 
-router.get('/:id', function (req, res) {
-  userModel.find({'_id': req.params.id}, function (err, user) {
-    if (err)
+router.get('/:currentUser', function (req, res) {
+  userModel.findOne({'username': req.params.currentUser}, function (err, user) {
+    if (err) {
       res.send(err);
-    res.json(user);
+    } else {
+      res.json(user);
+    }
   });
 });
 
-router.put('/:id', function (req, res) {
-  userModel.findOneAndUpdate({"_id": req.body._id}, {
-    "username": req.body.username, "email": req.body.email, "password": req.body.password,
-    "confirmpassword": req.body.confirmpassword, "date": req.body.date
+router.put('/:currentUser', function (req, res) {
+  userModel.findOneAndUpdate({'username': req.params.currentUser}, {
+    "username": req.body.username,
+    "email": req.body.email,
+    "password": req.body.password,
+    "confirmpassword": req.body.confirmpassword,
+    "date": req.body.date
   }, function (err, user) {
     if (err)
       res.send(err);
