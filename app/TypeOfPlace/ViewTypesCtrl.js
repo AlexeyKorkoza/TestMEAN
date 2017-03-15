e1baeb956d8b9f6ebc95384f4ae3af41e831733d
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-03-15 20:33:16 +0300

    Update view after query

--- app/TypeOfPlace/ViewTypesCtrl.js
+++ app/TypeOfPlace/ViewTypesCtrl.js
@@ -8,15 +8,16 @@ angular
     typeService.getAllTypes()
       .then(function (response) {
         $scope.types = response.data;
-        if($scope.types.length < 1) {
+        if ($scope.types.length < 1) {
           swal("Список типов объектов пуст", "Пожалуйста, добавьте новый тип объекта", "error");
           $location.url('/types/add');
         }
       });
 
-    $scope.delete = function (id) {
-      typeService.delete(id).then(function (response) {
+    $scope.delete = function (id, index) {
+      typeService.delete(id).then(function () {
         swal("Тип успешно удален", "Пожалуйста, нажмите ОК для продолжения", "success");
+        $scope.types.splice(index, 1);
       });
     };
 
