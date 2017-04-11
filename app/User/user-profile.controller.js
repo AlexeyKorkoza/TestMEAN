56a83dfd09b1209845ef513aaae93f92b9d74137
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-04-11 19:36:36 +0300

    Rewrite User by angular-styleguide

--- app/User/user-profile.controller.js
+++ app/User/user-profile.controller.js
@@ -2,43 +2,54 @@
 
 angular
   .module("myApp")
-  .controller(
-    "userProfileCtrl",
-    function($scope, $location, $routeParams, userService) {
-      userService.getUserInfo($routeParams.id).then(function(response) {
-        $scope.userData = response.data;
-      });
+  .controller("userProfileCtrl", userProfileCtrl);
 
-      $scope.update = function() {
-        userService
-          .updateUserInfo($routeParams.id, $scope.userData)
-          .then(function(response) {
-            if (response.data.code) {
-              swal("Информация не обновлена", "Повторите попытку", "error");
-            } else {
-              swal(
-                "Информация успешно обновлена",
-                "После выхода данные будут успешные применены",
-                "success"
-              );
-            }
-          });
-      };
+userProfileCtrl.$inject = ['$routeParams', 'userService'];
 
-      $scope.updatePassword = function() {
-        var data = {
-          username: $scope.userData.username,
-          email: $scope.userData.email,
-          password: $scope.settingPassword.password,
-          date: $scope.userData.date
-        };
-        userService.updateUserInfo($routeParams.id, data).then(function() {
+function userProfileCtrl($routeParams, userService) {
+
+  var vm = this;
+  vm.activate = activate;
+  vm.update = update;
+  vm.updatePassword = updatePassword;
+
+  activate();
+
+  function activate() {
+    userService.getUserInfo($routeParams.id).then(function (response) {
+      vm.userData = response.data;
+    });
+  }
+
+  function update() {
+    userService
+      .updateUserInfo($routeParams.id, vm.userData)
+      .then(function (response) {
+        if (response.data.code) {
+          swal("Информация не обновлена", "Повторите попытку", "error");
+        } else {
           swal(
             "Информация успешно обновлена",
             "После выхода данные будут успешные применены",
             "success"
           );
-        });
-      };
-    }
-  );
+        }
+      });
+  };
+
+  function updatePassword() {
+    var data = {
+      username: vm.userData.username,
+      email: vm.userData.email,
+      password: vm.settingPassword.password,
+      date: vm.userData.date
+    };
+    userService.updateUserInfo($routeParams.id, data).then(function () {
+      swal(
+        "Информация успешно обновлена",
+        "После выхода данные будут успешные применены",
+        "success"
+      );
+    });
+  };
+}
--- app/User/user.factory.js
+++ app/User/user.factory.js
@@ -1,14 +1,27 @@
-angular.module("myApp").factory("userService", function($http) {
-  return {
-    getUserInfo: function(id) {
-      return $http({
-        url: "/users/" + id,
-        method: "get"
-      });
-    },
+angular
+  .module("myApp")
+  .factory("userService", userService);
 
-    updateUserInfo: function(username, data) {
-      return $http.put("/users/" + username, data);
-    }
+userService.$inject = ['$http'];
+
+function userService($http) {
+
+  var service = {
+    getUserInfo: getUserInfo,
+    updateUserInfo: updateUserInfo
   };
-});
+
+  return service;
+
+  function getUserInfo(id) {
+    return $http({
+      url: "/users/" + id,
+      method: "get"
+    });
+  }
+
+  function updateUserInfo(username, data) {
+    return $http.put("/users/" + username, data);
+  }
+
+}
