386eac891883f603c2f89f0e809cac7ad61efb1b
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-09-23 23:40:27 +0300

    Add .eslintignore

--- gulpfile.js
+++ gulpfile.js
@@ -9,79 +9,79 @@ const csslint = require('gulp-csslint');
 const nodemon = require('gulp-nodemon');
 
 const js = [
-  'node_modules/jquery/dist/jquery.min.js',
-  'node_modules/angular/angular.min.js',
-  'node_modules/angular-route/angular-route.min.js',
-  'node_modules/selectize/dist/js/standalone/selectize.min.js',
-  'node_modules/angular-loading-bar/build/loading-bar.min.js',
-  'node_modules/ng-file-upload/dist/ng-file-upload.min.js',
-  'node_modules/ng-file-upload/dist/ng-file-upload-shim.min.js',
-  'node_modules/leaflet/dist/leaflet.js',
-  'node_modules/bootstrap/dist/js/bootstrap.min.js',
-  'node_modules/angular-selectize2/dist/selectize.js',
-  'node_modules/sweetalert/dist/sweetalert.min.js',
-  'node_modules/angular-simple-logger/dist/angular-simple-logger.js',
-  'node_modules/ui-leaflet/dist/ui-leaflet.min.js',
-  'node_modules/leaflet.markercluster/dist/leaflet.markercluster.js',
-  './app/assets/js/*.js',
-  './app/app.module.js',
-  './app/app.routes.js',
-  './app/jwt.factory.js',
-  './app/services/*.js',
-  './app/directivies/*.js',
-  './app/SignIn/*.js',
-  './app/Main/*.js',
-  './app/SignUp/*.js',
-  './app/Place/*.js',
-  './app/Type/*.js',
-  './app/Profile/*.js'
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-route/angular-route.min.js',
+    'node_modules/selectize/dist/js/standalone/selectize.min.js',
+    'node_modules/angular-loading-bar/build/loading-bar.min.js',
+    'node_modules/ng-file-upload/dist/ng-file-upload.min.js',
+    'node_modules/ng-file-upload/dist/ng-file-upload-shim.min.js',
+    'node_modules/leaflet/dist/leaflet.js',
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'node_modules/angular-selectize2/dist/selectize.js',
+    'node_modules/sweetalert/dist/sweetalert.min.js',
+    'node_modules/angular-simple-logger/dist/angular-simple-logger.js',
+    'node_modules/ui-leaflet/dist/ui-leaflet.min.js',
+    'node_modules/leaflet.markercluster/dist/leaflet.markercluster.js',
+    './app/assets/js/*.js',
+    './app/app.module.js',
+    './app/app.routes.js',
+    './app/jwt.factory.js',
+    './app/services/*.js',
+    './app/directivies/*.js',
+    './app/SignIn/*.js',
+    './app/Main/*.js',
+    './app/SignUp/*.js',
+    './app/Place/*.js',
+    './app/Type/*.js',
+    './app/Profile/*.js'
 ];
 
 const css = [
-  'node_modules/bootstrap/dist/css/bootstrap.min.css',
-  'node_modules/normalize.css/normalize.css',
-  'node_modules/leaflet/dist/leaflet.css',
-  'node_modules/selectize/dist/css/selectize.default.css',
-  'node_modules/sweetalert/dist/sweetalert.css',
-  'node_modules/angular-loading-bar/build/loading-bar.min.css',
-  'node_modules/leaflet.markercluster/dist/*.css',
-  './app/assets/css/style.css',
-  './app/assets/css/fonts.css'
+    'node_modules/bootstrap/dist/css/bootstrap.min.css',
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/leaflet/dist/leaflet.css',
+    'node_modules/selectize/dist/css/selectize.default.css',
+    'node_modules/sweetalert/dist/sweetalert.css',
+    'node_modules/angular-loading-bar/build/loading-bar.min.css',
+    'node_modules/leaflet.markercluster/dist/*.css',
+    './app/assets/css/style.css',
+    './app/assets/css/fonts.css'
 ];
 
 gulp.task('js', () => {
-  return gulp
-    .src(js)
-    .pipe(concat('build.js'))
-    .pipe(gulp.dest('./build/'))
-    .pipe(livereload());
+    return gulp
+        .src(js)
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('./build/'))
+        .pipe(livereload());
 });
 
 gulp.task('css', () => {
-  return gulp
-    .src(css)
-    .pipe(
-      autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-      })
-    )
-    .pipe(cssmin())
-    .pipe(csslint())
-    .pipe(concat('build.css'))
-    .pipe(gulp.dest('./build/'))
-    .pipe(livereload());
+    return gulp
+        .src(css)
+        .pipe(
+            autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            })
+        )
+        .pipe(cssmin())
+        .pipe(csslint())
+        .pipe(concat('build.css'))
+        .pipe(gulp.dest('./build/'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', ['js'], () => {
-  gulp.watch('app/**/*.js', ['js'])
+    gulp.watch('app/**/*.js', ['js']);
 });
 
 gulp.task('server', () => {
-  nodemon({
-    script: 'server.js',
-    env: { NODE_ENV: 'development' }
-  });
+    nodemon({
+        script: 'server.js',
+        env: { NODE_ENV: 'development' }
+    });
 });
 
 gulp.task('start', ['server', 'css', 'js', 'watch']);
--- karma.conf.js
+++ karma.conf.js
@@ -1,51 +1,51 @@
 module.exports = config => {
-  config.set({
-    basePath: "",
+    config.set({
+        basePath: '',
 
-    frameworks: ["jasmine"],
+        frameworks: ['jasmine'],
 
-    files: [
-      "node_modules/jquery/dist/jquery.min.js",
-      "node_modules/angular/angular.js",
-      "node_modules/angular-mocks/angular-mocks.js",
-      "node_modules/angular-route/angular-route.min.js",
-      "node_modules/selectize/dist/js/standalone/selectize.min.js",
-      "node_modules/angular-loading-bar/build/loading-bar.min.js",
-      "node_modules/ng-file-upload/dist/ng-file-upload.min.js",
-      "node_modules/ng-file-upload/dist/ng-file-upload-shim.min.js",
-      "node_modules/angular-selectize2/dist/selectize.js",
-      "node_modules/angular-simple-logger/dist/angular-simple-logger.js",
-      "node_modules/ui-leaflet/dist/ui-leaflet.min.js",
-      "app/assets/js/*.js",
+        files: [
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/angular/angular.js',
+            'node_modules/angular-mocks/angular-mocks.js',
+            'node_modules/angular-route/angular-route.min.js',
+            'node_modules/selectize/dist/js/standalone/selectize.min.js',
+            'node_modules/angular-loading-bar/build/loading-bar.min.js',
+            'node_modules/ng-file-upload/dist/ng-file-upload.min.js',
+            'node_modules/ng-file-upload/dist/ng-file-upload-shim.min.js',
+            'node_modules/angular-selectize2/dist/selectize.js',
+            'node_modules/angular-simple-logger/dist/angular-simple-logger.js',
+            'node_modules/ui-leaflet/dist/ui-leaflet.min.js',
+            'app/assets/js/*.js',
 
-      "app/*.js",
-      "app/Main/main.controller.js",
-      "app/Place/*.js",
-      "app/SignIn/*.js",
-      "app/SignUp/*.js",
-      "app/Type/*.js",
-      "app/services/*.js",
-      "app/test/*.js"
-    ],
+            'app/*.js',
+            'app/Main/main.controller.js',
+            'app/Place/*.js',
+            'app/SignIn/*.js',
+            'app/SignUp/*.js',
+            'app/Type/*.js',
+            'app/services/*.js',
+            'app/test/*.js'
+        ],
 
-    exclude: [],
+        exclude: [],
 
-    preprocessors: {},
+        preprocessors: {},
 
-    reporters: ["progress"],
+        reporters: ['progress'],
 
-    port: 9876,
+        port: 9876,
 
-    colors: true,
+        colors: true,
 
-    logLevel: config.LOG_INFO,
+        logLevel: config.LOG_INFO,
 
-    autoWatch: true,
+        autoWatch: true,
 
-    browsers: ["Chrome"],
+        browsers: ['Chrome'],
 
-    singleRun: false,
+        singleRun: false,
 
-    concurrency: Infinity
-  });
+        concurrency: Infinity
+    });
 };
