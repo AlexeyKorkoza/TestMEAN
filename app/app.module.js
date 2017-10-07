1cb31c7634f3a612cb95d6c55a9e08a4cade039c
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-10-07 16:35:50 +0300

    Add eslint for client side

--- app/app.module.js
+++ app/app.module.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict';
 
 angular
-  .module("myApp", [
-    "ngRoute",
-    "selectize",
-    "angular-loading-bar",
-    "ngAnimate",
-    "ngFileUpload",
-    "wt.responsive",
-    "ui-leaflet"
-  ])
-  .config(config)
-  .run(run);
+    .module('myApp', [
+        'ngRoute',
+        'selectize',
+        'angular-loading-bar',
+        'ngAnimate',
+        'ngFileUpload',
+        'wt.responsive',
+        'ui-leaflet'
+    ])
+    .config(config)
+    .run(run);
 
-config.$inject = ["cfpLoadingBarProvider"];
-run.$inject = ["$http", "userService"];
+config.$inject = ['cfpLoadingBarProvider'];
+run.$inject = ['$http', 'userService'];
 
 function config(cfpLoadingBarProvider) {
-  cfpLoadingBarProvider.latencyThreshold = 1000;
+    cfpLoadingBarProvider.latencyThreshold = 1000;
 }
 
 function run($http, userService) {
   
-    if(localStorage.getItem("username")){
-      userService.getUser().then(function(response){
-        localStorage.setItem("username", response.data.user.username);
-        localStorage.setItem("token", response.data.user.token);
-      })
+    if(localStorage.getItem('username')){
+        userService.getUser().then(function(response){
+            localStorage.setItem('username', response.data.user.username);
+            localStorage.setItem('token', response.data.user.token);
+        });
     }
 }
--- app/app.routes.js
+++ app/app.routes.js
@@ -1,41 +1,41 @@
-"use strict";
+'use strict';
 
-angular.module("myApp")
-  .config(routeConfig);
+angular.module('myApp')
+    .config(routeConfig);
 
-routeConfig.$inject = ["$routeProvider"];
+routeConfig.$inject = ['$routeProvider'];
 
 function routeConfig($routeProvider) {
-  $routeProvider
-    .when("/", {
-      templateUrl: "app/Main/main.html"
-    })
-    .when("/signin", {
-      templateUrl: "app/SignIn/sign-in.html"
-    })
-    .when("/signup", {
-      templateUrl: "app/SignUp/sign-up.html"
-    })
-    .when("/user/edit", {
-      templateUrl: "app/Profile/profile.html"
-    })
-    .when("/types", {
-      templateUrl: "app/Type/view-types.html"
-    })
-    .when("/types/add", {
-      templateUrl: "app/Type/add-type.html"
-    })
-    .when("/types/:id", {
-      templateUrl: "app/Type/edit-type.html"
-    })
-    .when("/places", {
-      templateUrl: "app/Place/view-places.html"
-    })
-    .when("/places/add", {
-      templateUrl: "app/Place/add-place.html"
-    })
-    .when("/places/:id", {
-      templateUrl: "app/Place/edit-place.html"
-    })
-    .otherwise({redirectTo: "/"});
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/Main/main.html'
+        })
+        .when('/signin', {
+            templateUrl: 'app/SignIn/sign-in.html'
+        })
+        .when('/signup', {
+            templateUrl: 'app/SignUp/sign-up.html'
+        })
+        .when('/user/edit', {
+            templateUrl: 'app/Profile/profile.html'
+        })
+        .when('/types', {
+            templateUrl: 'app/Type/view-types.html'
+        })
+        .when('/types/add', {
+            templateUrl: 'app/Type/add-type.html'
+        })
+        .when('/types/:id', {
+            templateUrl: 'app/Type/edit-type.html'
+        })
+        .when('/places', {
+            templateUrl: 'app/Place/view-places.html'
+        })
+        .when('/places/add', {
+            templateUrl: 'app/Place/add-place.html'
+        })
+        .when('/places/:id', {
+            templateUrl: 'app/Place/edit-place.html'
+        })
+        .otherwise({redirectTo: '/'});
 }
