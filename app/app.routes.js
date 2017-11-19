e467ff5ed6b54dbfdf61252b97ba827c64293ed8
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-11-19 10:59:57 +0300

    #24 - Add properties in routes

--- app/app.routes.js
+++ app/app.routes.js
@@ -22,38 +22,56 @@ function routeConfig($stateProvider, $urlRouterProvider) {
         })
         .state('signin', {
             url: '/signin',
-            templateUrl: 'app/SignIn/sign-in.html'
+            templateUrl: 'app/SignIn/sign-in.html',
+            controller: 'signInCtrl',
+            controllerAs: 'vm'
         })
         .state('signup', {
             url: '/signup',
-            templateUrl: 'app/SignUp/sign-up.html'
+            templateUrl: 'app/SignUp/sign-up.html',
+            controller: 'signUpCtrl',
+            controllerAs: 'vm'
         })
         .state('profile', {
             url: '/profile/{id}',
-            templateUrl: 'app/Profile/profile.html'
+            templateUrl: 'app/Profile/profile.html',
+            controller: 'userProfileCtrl',
+            controllerAs: 'vm'
         })
         .state('types_all', {
             url: '/types',
-            templateUrl: 'app/Type/view-types.html'
+            templateUrl: 'app/Type/view-types.html',
+            controller: 'viewTypesCtrl',
+            controllerAs: 'vm'
         })
         .state('types_new', {
             url: '/types/add',
-            templateUrl: 'app/Type/add-type.html'
+            templateUrl: 'app/Type/add-type.html',
+            controller: 'addTypeCtrl',
+            controllerAs: 'vm'
         })
         .state('types_one', {
             url: '/types/:id',
-            templateUrl: 'app/Type/edit-type.html'
+            templateUrl: 'app/Type/edit-type.html',
+            controller: 'editTypeCtrl',
+            controllerAs: 'vm'
         })
         .state('places_all', {
             url: '/places',
-            templateUrl: 'app/Place/view-places.html'
+            templateUrl: 'app/Place/view-places.html',
+            controller: 'viewPlacesCtrl',
+            controllerAs: 'vm'
         })
         .state('places_new', {
             url: '/places/add',
-            templateUrl: 'app/Place/add-place.html'
+            templateUrl: 'app/Place/add-place.html',
+            controller: 'addPlaceCtrl',
+            controllerAs: 'vm'
         })
         .state('places_one', {
             url: '/places/:id',
-            templateUrl: 'app/Place/edit-place.html'
+            templateUrl: 'app/Place/edit-place.html',
+            controller: 'editPlaceCtrl',
+            controllerAs: 'vm'
         })
 }
