db8a1005eda8b04a7324377d45af93174d7a9270
AlexeyKorkoza <alexeykorkoza@gmail.com>
2017-12-17 11:50:07 +0300

    Remove unused route

--- app/routes/app.js
+++ app/routes/app.js
@@ -3,13 +3,11 @@
 import express from 'express';
 import places from './places';
 import types from './types';
-import users from './users';
 import profile from './profile';
 const router = express();
 
 router.use('/places', places);
 router.use('/types', types);
-router.use('/user', users);
 router.use('/profile', profile);
 
 export default router;
