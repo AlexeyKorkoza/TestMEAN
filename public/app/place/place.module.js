3320d2f89d0fd9f20626470d72c19c0137b4ffbe
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-01-14 15:58:44 +0300

    Update module

--- public/app/place/place.module.js
+++ public/app/place/place.module.js
@@ -1,16 +1,12 @@
 import angular from 'angular';
 
 import listCtrl from './controllers/list-places.controller';
-import addCtrl from './controllers/add-place.controller';
-import editCtrl from './controllers/edit-place.controller';
 import editorPlaceCtrl from './controllers/editor-place.controller';
 import service from './services/place.service';
 import routes from './place.routes';
 
 export default angular.module('myApp.places', [])
     .controller('listPlacesCtrl', listCtrl)
-    .controller('addPlaceCtrl', addCtrl)
-    .controller('editPlaceCtrl', editCtrl)
     .controller('editorPlaceCtrl', editorPlaceCtrl)
     .factory('placeService', service)
     .config(routes)
