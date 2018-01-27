774c567f5998b447222469d8f51592f1a4714355
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-01-27 19:59:37 +0300

    Update models

--- app/models/place.js
+++ app/models/place.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const Place = mongoose.model('Place', {
-  name_place: String,
+  name: String,
   description: String,
   lat: String,
   lng: String,
--- app/models/type.js
+++ app/models/type.js
@@ -2,10 +2,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const Type = mongoose.model('Type', {
-  name_type: {
-    type: String,
-    unique: true
-  },
+  name: String,
   image: String,
   date: {
     type: Date,
