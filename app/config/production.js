33f72daab32e88eaef64e26e5857ef7e9aee64c4
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-04-16 14:47:30 +0300

    Add connection to production db

--- app/config/production.js
+++ app/config/production.js
@@ -11,6 +11,8 @@ module.exports = {
         host: process.env.DB_HOST || '127.0.0.1',
         dbName: process.env.DB_NAME || 'interactive_map',
         port: process.env.DB_PORT || 27017,
+        userName: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
     },
     jwtSecret: process.env.JWT_SECRET,
     session: {
