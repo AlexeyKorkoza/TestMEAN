cba386742bdcb598931c432622d26c02d16cc74f
AlexeyKorkoza <alexeykorkoza@gmail.com>
2018-04-17 20:21:52 +0300

    Fix urls

--- copy_files.js
+++ copy_files.js
@@ -1,4 +1,4 @@
 /* Copy all views */
 const fs = require('fs-extra');
 
-fs.copy('app/views', 'dist/views').then(() => console.log('Files were copying'));
+fs.copy('app/views', 'app/views').then(() => console.log('Files were copying'));
